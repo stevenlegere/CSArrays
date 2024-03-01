988ee63e3b3a418f7ae4fe4ecfa4736b7f14a21d
stevenlegere <stevenmarklegere@gmail.com>
2024-03-01 21:03:45 +0000

    Working with array to assign random letter and numbers

--- Program.cs
+++ Program.cs
@@ -74,3 +74,24 @@ foreach (string orderID in OrderIDs)
 
         Console.WriteLine(orderID);
 }
+
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    //Combine the prefix and sufix togetherm then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
